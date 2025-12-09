48e680904781275e90d3bd961f94e811b4894757
Darkbound <todor@alphabite.io>
2025-12-09 11:08:47 +0200

    build fix

--- src/modules/wishlist/models/wishlist-item.ts
+++ src/modules/wishlist/models/wishlist-item.ts
@@ -1,7 +1,6 @@
 import { model } from "@medusajs/framework/utils";
 import { InferTypeOf } from "@medusajs/framework/types";
 import { Wishlist } from "./wishlist";
-import { InferTypeOf } from "@medusajs/framework/types";
 
 export const WishlistItem = model
   .define("wishlist_item", {
