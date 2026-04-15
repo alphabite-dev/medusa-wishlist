4fef4052e7f47788e4197490aaf6abf3c724c342
Ivan <ivan@alphabite.io>
2026-04-15 19:17:06 +0300

    update migration file

--- src/modules/wishlist/migrations/Migration20260415155648.ts
+++ src/modules/wishlist/migrations/Migration20260415155648.ts
@@ -1,12 +1,17 @@
 import { Migration } from "@mikro-orm/migrations";
-
 export class Migration20260415155648 extends Migration {
   override async up(): Promise<void> {
     this.addSql(
-      `alter table if exists "wishlist_item" add column if not exists "product_id" text not null;`,
+      `alter table if exists "wishlist_item" add column if not exists "product_id" text;`,
+    );
+
+    this.addSql(
+      `update "wishlist_item" set "product_id" = 'temp_placeholder' where "product_id" is null;`,
+    );
+    this.addSql(
+      `alter table if exists "wishlist_item" alter column "product_id" set not null;`,
     );
   }
-
   override async down(): Promise<void> {
     this.addSql(
       `alter table if exists "wishlist_item" drop column if exists "product_id";`,
