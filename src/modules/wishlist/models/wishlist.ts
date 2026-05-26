36f0847bf6a4a8e4c93159d6f0bf301a339932b0
Ivan <ivan@alphabite.io>
2026-05-26 18:49:20 +0300

    fix delete wishlist bug

--- src/modules/wishlist/models/wishlist.ts
+++ src/modules/wishlist/models/wishlist.ts
@@ -10,6 +10,9 @@ export const Wishlist = model
     sales_channel_id: model.text(),
     items: model.hasMany(() => WishlistItem),
   })
+  .cascades({
+    delete: ["items"],
+  })
   .indexes([
     {
       on: ["customer_id", "sales_channel_id"],
